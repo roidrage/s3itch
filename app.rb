451755d7ba5aa1d2982cb13384d7df2f53b67643
Matias Korhonen <matias@kiskolabs.com>
2012-09-29 19:40:30 +0300

    Fixes a typo

--- app.rb
+++ app.rb
@@ -22,7 +22,7 @@ class S3itchApp < Sinatra::Base
     begin
       # Skitch does not encode question marks, so we have to recombine the
       # name here if necessary
-      name = request.query_string ? 
+      name = if request.query_string
         "#{params[:name]}?#{request.query_string}"
       else
         params[:name]
